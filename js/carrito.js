0701581b7923d0cb123987faf67bab6be1903793
Tomas Gonzalez <gonzaleztomasn@hotmail.com>
2023-03-20 11:26:18 -0300

    "modificacion"

--- js/carrito.js
+++ js/carrito.js
@@ -1,37 +1,3 @@
-const contenidoTienda = document.getElementById("contenidoTienda")
-const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("modal-container")
-let saludo = prompt("Hola, ingresa tu nombre y apellido por favor")
-let carrito = [];
-
-productos.forEach((product) => {
-    let content = document.createElement("div");
-    content.className = "card";
-    content.innerHTML = `
-    <img src="${product.img}"
-    <h3>${product.nombre}</h3>
-    <p class="precio">$ ${product.precio}</p>
-    `;
-
-    contenidoTienda.append(content);
-
-    let comprar = document.createElement("button")
-    comprar.innerText = "comprar";
-    comprar.className = "comprar";
-
-    content.append(comprar);
-
-    comprar.addEventListener("click", () => {
-        carrito.push({
-            id: product.id,
-            img: product.img,
-            nombre: product.nombre,
-            precio: product.precio,
-        })
-        console.log(carrito);
-    })
-});
-
 verCarrito.addEventListener("click", () => {
     modalContainer.innerHTML = "";
     modalContainer.style.display = "flex";
